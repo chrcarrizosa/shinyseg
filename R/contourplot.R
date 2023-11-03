1269beabab60936de1d2d068749d03f30e827de9
Christian Carrizosa <chrcarrizosa@gmail.com>
2023-11-03 22:54:28 +0100

    Fix minor code grammar issues

--- R/contourplot.R
+++ R/contourplot.R
@@ -1,38 +1,38 @@
-# Functions for displaying the FLB on a gradient colour bar
+# Functions for displaying the FLB on a contour plot
 
 library(ggplot2)
 library(metR)
 
-craftbrewer_pal <- function (type = "seq", palette = 1, direction = 1) 
+craftbrewer_pal = function (type = "seq", palette = 1, direction = 1) 
 {
-  pal <- scales:::pal_name(palette, type)
+  pal = scales:::pal_name(palette, type)
   force(direction)
   function(n) {
-    n_max_palette <- RColorBrewer:::maxcolors[names(RColorBrewer:::maxcolors) == palette]
+    n_max_palette = RColorBrewer:::maxcolors[names(RColorBrewer:::maxcolors) == palette]
     
     if (n < 3) {
-      pal <- suppressWarnings(RColorBrewer::brewer.pal(n, pal))
+      pal = suppressWarnings(RColorBrewer::brewer.pal(n, pal))
     } else if (n > n_max_palette){
       rlang::warn(paste(n, "colours used, but", palette, "has only",
                         n_max_palette, "- New palette created based on all colors of", 
                         palette))
-      n_palette <- RColorBrewer::brewer.pal(n_max_palette, palette)
-      colfunc <- grDevices::colorRampPalette(n_palette)
-      pal <- colfunc(n)
+      n_palette = RColorBrewer::brewer.pal(n_max_palette, palette)
+      colfunc = grDevices::colorRampPalette(n_palette)
+      pal = colfunc(n)
     }
     else {
-      pal <- RColorBrewer::brewer.pal(n, pal)
+      pal = RColorBrewer::brewer.pal(n, pal)
     }
-    pal <- pal[seq_len(n)]
+    pal = pal[seq_len(n)]
     if (direction == -1) {
-      pal <- rev(pal)
+      pal = rev(pal)
     }
     pal
   }
 }
 
-scale_fill_craftfermenter <- function(..., type = "seq", palette = 1, direction = -1, na.value = "grey50", guide = "coloursteps", aesthetics = "fill") {
-  type <- match.arg(type, c("seq", "div", "qual"))
+scale_fill_craftfermenter = function(..., type = "seq", palette = 1, direction = -1, na.value = "grey50", guide = "coloursteps", aesthetics = "fill") {
+  type = match.arg(type, c("seq", "div", "qual"))
   if (type == "qual") {
     warn("Using a discrete colour palette in a binned scale.\n  Consider using type = \"seq\" or type = \"div\" instead")
   }
