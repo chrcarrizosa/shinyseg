af415d87ab81df54b9890d472b3767d0db5757d9
chrcarrizosa <chrcarrizosa@gmail.com>
2024-04-19 14:29:50 +0200

    Restore contour plot post ggplot2 updates

--- R/contourplot.R
+++ R/contourplot.R
@@ -3,42 +3,11 @@
 library(ggplot2)
 library(metR)
 
-craftbrewer_pal = function (type = "seq", palette = 1, direction = 1) 
-{
-  pal = scales:::pal_name(palette, type)
-  force(direction)
-  function(n) {
-    n_max_palette = RColorBrewer:::maxcolors[names(RColorBrewer:::maxcolors) == palette]
-    
-    if (n < 3) {
-      pal = suppressWarnings(RColorBrewer::brewer.pal(n, pal))
-    } else if (n > n_max_palette){
-      rlang::warn(paste(n, "colours used, but", palette, "has only",
-                        n_max_palette, "- New palette created based on all colors of", 
-                        palette))
-      n_palette = RColorBrewer::brewer.pal(n_max_palette, palette)
-      colfunc = grDevices::colorRampPalette(n_palette)
-      pal = colfunc(n)
-    }
-    else {
-      pal = RColorBrewer::brewer.pal(n, pal)
-    }
-    pal = pal[seq_len(n)]
-    if (direction == -1) {
-      pal = rev(pal)
-    }
-    pal
-  }
+RdBu = function(name = waiver(), ..., values = NULL, space = "Lab", 
+                na.value = "grey50", guide = "coloursteps", aesthetics = "fill") {
+  pal = ggplot2:::pal_binned(function(n) rev(RColorBrewer::brewer.pal(n, "RdBu")))
+  binned_scale(aesthetics, name = name, palette = pal, na.value = na.value, guide = guide, ...)
 }
-
-scale_fill_craftfermenter = function(..., type = "seq", palette = 1, direction = -1, na.value = "grey50", guide = "coloursteps", aesthetics = "fill") {
-  type = match.arg(type, c("seq", "div", "qual"))
-  if (type == "qual") {
-    warn("Using a discrete colour palette in a binned scale.\n  Consider using type = \"seq\" or type = \"div\" instead")
-  }
-  binned_scale(aesthetics, "fermenter", ggplot2:::binned_pal(craftbrewer_pal(type, palette, direction)), na.value = na.value, guide = guide, ...)
-}
-
 
 # Contour plot
 contourplot = function(grid, current, values) {
@@ -52,10 +21,10 @@ contourplot = function(grid, current, values) {
       x = names(grid)[1],
       y = names(grid)[2]
     ) +
-    scale_fill_craftfermenter(
+    scale_fill_binned(
       breaks = c(-5, -4, -3, -2, 0, 2, 3, 4, 5),
       labels = c("", "", "", "", "1", "4", "8", "16", "32"),
-      palette = "RdBu",
+      type = RdBu,
       limits = c(-6, +6)
     ) +
     theme_classic() +
